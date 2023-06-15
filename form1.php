24674c687891acb12c633f61df3f427a15f36430
aka5hbansal <akashbansal0867@gmail.com>
2023-06-16 05:00:10 +0530

    front end development

---
+++ form1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>software development</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="centre">
+        <h1>Employee Data Entry automation software</h1>
+        <div class="form">
+            <input type="text" class="textfield" placeholder="search id">
+            <input type="text" class="textfield" placeholder="Employee name">
+            <select>
+                <option>select gender</option>
+                <option>male</option>
+                <option>female</option> 
+            </select>    
+            <input type="text" class="textfield" placeholder="Email address">
+            <select>
+                <option>select department</option>
+                <option>It</option>
+                <option>Accounts</option>
+                <option>Sales</option>
+                <option>HR</option>
+                <option>Technical</option> 
+            </select>
+            <input type="text" class="textfield" placeholder="search id">
+            <textarea placeholder="address"></textarea>
+            <input type="button" value="search" name="" class="btn">
+    </div>
+</body>
+</html>
