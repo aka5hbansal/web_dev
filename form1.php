6b2b57ff9b9c3482d2cd07d0172c421dbf4f2b64
aka5hbansal <akashbansal0867@gmail.com>
2023-06-16 05:37:35 +0530

    front end complete

--- form1.php
+++ form1.php
@@ -12,13 +12,13 @@
         <div class="form">
             <input type="text" class="textfield" placeholder="search id">
             <input type="text" class="textfield" placeholder="Employee name">
-            <select>
+            <select class="textfield">
                 <option>select gender</option>
                 <option>male</option>
                 <option>female</option> 
             </select>    
             <input type="text" class="textfield" placeholder="Email address">
-            <select>
+            <select class="textfield">
                 <option>select department</option>
                 <option>It</option>
                 <option>Accounts</option>
@@ -26,9 +26,13 @@
                 <option>HR</option>
                 <option>Technical</option> 
             </select>
-            <input type="text" class="textfield" placeholder="search id">
+    
             <textarea placeholder="address"></textarea>
             <input type="button" value="search" name="" class="btn">
+            <input type="button" value="save" name="" class="btn" style="background-color:green;">
+            <input type="button" value="modify" name="" class="btn" style="background-color:orange;">
+            <input type="button" value="Delete" name="" class="btn" style="background-color:red;">
+            <input type="button" value="clear" name="" class="btn" style="background-color:blue;">
     </div>
 </body>
 </html>
