2ae061ca653f9652d1bcf6b7ffedd4306916d546
aka5hbansal <akashbansal0867@gmail.com>
2023-06-16 08:16:15 +0530

    search and save button function colpleted

--- form1.php
+++ form1.php
@@ -1,3 +1,20 @@
+<?php
+include("connection.php");
+?>
+<?php
+if(isset($_POST['search']))
+{
+    $search      =$_POST['search'];
+    
+    
+    $query  = "select * from form where id='$search'";
+
+    $data=mysqli_query($conn,$query);
+    $result=mysqli_fetch_assoc($data);
+    //$name=$result['emp_name'];
+    //echo $name;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,35 +25,55 @@
 </head>
 <body>
     <div class="centre">
+        <form action="#" method="POST">
         <h1>Employee Data Entry automation software</h1>
         <div class="form">
-            <input type="text" class="textfield" placeholder="search id">
-            <input type="text" class="textfield" placeholder="Employee name">
-            <select class="textfield">
-                <option>select gender</option>
-                <option>male</option>
-                <option>female</option> 
+            <input type="text" name="search" class="textfield" placeholder="search id" value="<?php if(isset($_POST['searchdata'])){echo $result['id'];}?>">
+            <input type="text" name="name" class="textfield" placeholder="Employee name" value="<?php if(isset($_POST['searchdata'])){echo $result['emp_name'];}?>">
+            <select class="textfield" name="gender">
+                <option value="not selected">select gender</option>
+                <option value="male" <?php if($result['emp_gender']=='male'){echo "selected";}?>>male</option>
+                <option value="female" <?php if($result['emp_gender']=='female'){echo "selected";}?>>female</option> 
             </select>    
-            <input type="text" class="textfield" placeholder="Email address">
-            <select class="textfield">
-                <option>select department</option>
-                <option>It</option>
-                <option>Accounts</option>
-                <option>Sales</option>
-                <option>HR</option>
-                <option>Technical</option> 
+            <input type="text" name="email" class="textfield" placeholder="Email address" value="<?php if(isset($_POST['searchdata'])){echo $result['emp_email'];}?>">
+            <select class="textfield" name="department">
+                <option value="not selected">select department</option>
+                <option value="IT" <?php if($result['emp_dept']=='IT'){echo "selected";}?>>It</option>
+                <option value="Accounts" <?php if($result['emp_dept']=='Accounts'){echo "selected";}?>>Accounts</option>
+                <option value="Sales" <?php if($result['emp_dept']=='Sales'){echo "selected";}?>>Sales</option>
+                <option value="HR" <?php if($result['emp_dept']=='HR'){echo "selected";}?>>HR</option>
+                <option value="Technical">Technical</option> 
             </select>
     
-            <textarea placeholder="address"></textarea>
-            <input type="button" value="search" name="" class="btn">
-            <input type="button" value="save" name="" class="btn" style="background-color:green;">
-            <input type="button" value="modify" name="" class="btn" style="background-color:orange;">
-            <input type="button" value="Delete" name="" class="btn" style="background-color:red;">
-            <input type="button" value="clear" name="" class="btn" style="background-color:blue;">
+            <textarea placeholder="address" name="address"><?php if(isset($_POST['searchdata'])){echo $result['emp_address'];}?></textarea>
+            <input type="submit" value="search" name="searchdata" class="btn">
+            <input type="submit" value="save" name="save" class="btn" style="background-color:green;">
+            <input type="submit" value="modify" name="" class="btn" style="background-color:orange;">
+            <input type="submit" value="Delete" name="" class="btn" style="background-color:red;">
+            <input type="submit" value="clear" name="" class="btn" style="background-color:blue;">
     </div>
+</form>
+</div>
 </body>
 </html>
 
 <?php
-include("connection.php");
+if(isset($_POST['save']))
+{
+    $name      =$_POST['name'];
+    $gender    =$_POST['gender'];
+    $email     =$_POST['email'];
+    $department=$_POST['department'];
+    $address   =$_POST['address'];
+    
+    $query  = "INSERT INTO form(emp_name,emp_gender,emp_email,emp_dept,emp_address) VALUES ('$name','$gender','$email','$department','$address')";
+
+    $data=mysqli_query($conn,$query);
+    if($data){
+        echo "Data saved into Database";
+    }
+    else{
+        echo "Failed to save data";
+    }
+}
 ?>
